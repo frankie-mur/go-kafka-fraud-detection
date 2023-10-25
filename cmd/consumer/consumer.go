dc499d4690a5d45074a207d0142c214a1a8df6e7
Frankie <frankidatank@gmail.com>
2023-10-24 23:04:20 -0700

    clean up structure, add comments

--- cmd/consumer/consumer.go
+++ cmd/consumer/consumer.go
@@ -16,54 +16,6 @@ const (
 	KafkaTopic         = "notifications"
 )
 
-func main() {
-	worker, err := connectConsumer()
-	if err != nil {
-		panic(err)
-	}
-
-	// Calling ConsumePartition. It will open one connection per broker
-	// and share it for all partitions that live on it.
-	// NOTE: setting offset to OffsetNewest, consumer will only receive messages after its start
-	// OffsetOldest will have all messages sent on start
-	consumer, err := worker.ConsumePartition(KafkaTopic, 0, sarama.OffsetNewest)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Consumer started ")
-	sigchan := make(chan os.Signal, 1)
-	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
-	// Get signal for finish
-	doneCh := make(chan struct{})
-
-	//spins up a goroutine to fetch messages from Kafka
-	//and handle them, while also monitoring for shutdown signals
-	go func() {
-		for {
-			select {
-			case err := <-consumer.Errors():
-				fmt.Println(err)
-			case msg := <-consumer.Messages():
-				err := handleMessage(msg)
-				if err != nil {
-					fmt.Printf("Failed to handle message for consumer with key: %v, with error: %v\n", msg.Key, err)
-				}
-			case <-sigchan:
-				fmt.Println("Interrupt is detected")
-				doneCh <- struct{}{}
-			}
-		}
-	}()
-
-	<-doneCh
-	fmt.Println("Processed all messages, awaiting more...")
-
-	if err := worker.Close(); err != nil {
-		panic(err)
-	}
-
-}
-
 func connectConsumer() (sarama.Consumer, error) {
 	config := sarama.NewConfig()
 	config.Consumer.Return.Errors = true
@@ -98,7 +50,55 @@ func handleMessage(msg *sarama.ConsumerMessage) error {
 
 func callVeritas(bankAccount models.BankAccount) bool {
 	fmt.Printf("Calling Veritas for %s bank...\n", bankAccount.FirstName)
-	//Simulate calling an actual service
+	//Simulate calling an actual verification service
 	//Here we just randomly return true or false
 	return rand.Intn(2) == 0
+}
+
+func main() {
+	worker, err := connectConsumer()
+	if err != nil {
+		panic(err)
+	}
+	// Calling ConsumePartition. It will open one connection per broker
+	// and share it for all partitions that live on it.
+	// NOTE: setting offset to OffsetNewest, consumer will only receive messages after its start
+	// OffsetOldest will have all messages sent on start
+	consumer, err := worker.ConsumePartition(KafkaTopic, 0, sarama.OffsetNewest)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Consumer started ")
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+	// Get signal for finish
+	doneCh := make(chan struct{})
+
+	//spins up a goroutine to fetch messages from Kafka
+	//and handle them, while also monitoring for shutdown signals
+	go func() {
+		for {
+			select {
+			case err := <-consumer.Errors():
+				fmt.Println(err)
+			case msg := <-consumer.Messages():
+				//Handle the message received  from Kafka
+				err := handleMessage(msg)
+				if err != nil {
+					fmt.Printf("Failed to handle message for consumer with key: %v, with error: %v\n", msg.Key, err)
+				}
+			case <-sigchan:
+				fmt.Println("Interrupt is detected")
+				doneCh <- struct{}{}
+			}
+		}
+	}()
+
+	<-doneCh
+	fmt.Println("Processed all messages, awaiting more...")
+
+	if err := worker.Close(); err != nil {
+		panic(err)
+	}
+
 }
